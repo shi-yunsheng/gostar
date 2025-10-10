4d122c5d1a0e92256eddb08934389a62809ed64a
sheng <shi-yunsheng@qq.com>
2025-10-10 15:40:23 +0800

    Refactor model binding: do not require FromJson implementation

--- router/handler/bind.go
+++ router/handler/bind.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"encoding/json"
-	"errors"
 	"reflect"
 	"sync"
 
@@ -13,9 +12,9 @@ import (
 //
 // @zh 绑定参数
 type Bind struct {
-	// @en model, must implement "FromJson(string) error" interface, can implement "Validate() error" interface, if "Validate" interface is implemented, it will be used to validate the model, otherwise use github.com/go-playground/validator/v10 to validate the model, for more information about validator, please refer to https://github.com/go-playground/validator
+	// @en model, can implement "Validate() error" interface, if "Validate" interface is implemented, it will be used to validate the model, otherwise use github.com/go-playground/validator/v10 to validate the model, for more information about validator, please refer to https://github.com/go-playground/validator
 	//
-	// @zh 模型，必须实现"FromJson(string) error"接口，可以选择实现"Validate() error"接口，如果有"Validate"接口，则优先使用"Validate"接口进行校验，否则使用 github.com/go-playground/validator/v10 进行校验，有关validator的用法请参考 https://github.com/go-playground/validator
+	// @zh 模型，可以实现"Validate() error"接口，如果有"Validate"接口，则优先使用"Validate"接口进行校验，否则使用 github.com/go-playground/validator/v10 进行校验，有关validator的用法请参考 https://github.com/go-playground/validator
 	Model any
 	// @en method, same as HTTP method, if empty, bind according to request method
 	//
@@ -66,38 +65,30 @@ func (b *Bind) Validate(r *Request) (any, error) {
 			model = query
 		}
 
-		// @en model must implement "FromJson(string) error" interface
-		// @zh 模型必须实现"FromJson(string) error"接口
-		if fromJsonObj, ok := modelInstance.(interface{ FromJson(string) error }); ok {
-			jsonData, err := json.Marshal(model)
+		jsonData, err := json.Marshal(model)
+		if err != nil {
+			return nil, err
+		}
+		err = json.Unmarshal(jsonData, modelInstance)
+		if err != nil {
+			return nil, err
+		}
+
+		// @en if model implements "Validate() error" interface, use Validate interface to validate the model
+		// @zh 如果模型实现了"Validate() error"接口，则使用Validate接口进行校验
+		if validateObj, ok := modelInstance.(interface{ Validate() error }); ok {
+			err = validateObj.Validate()
 			if err != nil {
 				return nil, err
 			}
-			err = fromJsonObj.FromJson(string(jsonData))
-			if err != nil {
-				return nil, err
-			}
-
-			// @en if model implements "Validate() error" interface, use Validate interface to validate the model
-			// @zh 如果模型实现了"Validate() error"接口，则使用Validate接口进行校验
-			if validateObj, ok := modelInstance.(interface{ Validate() error }); ok {
-				err = validateObj.Validate()
-				if err != nil {
-					return nil, err
-				}
-			} else {
-				// @en model does not implement "Validate() error" interface, use github.com/go-playground/validator/v10 to validate the model
-				// @zh 模型没有实现"Validate() error"接口，使用github.com/go-playground/validator/v10进行校验
-				validate := validator.New()
-				err = validate.Struct(fromJsonObj)
-				if err != nil {
-					return nil, err
-				}
-			}
-
-			modelInstance = fromJsonObj
 		} else {
-			return nil, errors.New(`model must implement "FromJson(string) error" interface`)
+			// @en model does not implement "Validate() error" interface, use github.com/go-playground/validator/v10 to validate the model
+			// @zh 模型没有实现"Validate() error"接口，使用github.com/go-playground/validator/v10进行校验
+			validate := validator.New()
+			err = validate.Struct(modelInstance)
+			if err != nil {
+				return nil, err
+			}
 		}
 	}
 	return modelInstance, nil

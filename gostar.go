a642407c489ecccfb7d9744290f2221224f4ebbd
sheng <shi-yunsheng@qq.com>
2025-11-13 23:14:35 +0800

    更新版本号

--- gostar.go
+++ gostar.go
@@ -24,7 +24,7 @@ type goStar struct {
 // 新建GoStar实例
 func New(configName ...string) *goStar {
 	instance := &goStar{
-		version: "1.0.16-beta",
+		version: "1.0.17-beta",
 		config:  getConfig(configName...),
 		router:  router.NewRouter(),
 	}

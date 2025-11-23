dcfc5dc4412650302926b9cbd66bb2d103668baa
sheng <shi-yunsheng@qq.com>
2025-11-23 12:57:41 +0800

    修复:连表查询时表名错误

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ GoStar 是一个轻量级、功能完善的 Go Web 框架，提供简明友好�
 
 ## 安装
 ```bash
-go get -u github.com/shi-yunsheng/gostar@v1.0.33-beta
+go get -u github.com/shi-yunsheng/gostar@v1.0.34-beta
 ```
 
 ## 快速开始
--- model/join.go
+++ model/join.go
@@ -7,6 +7,37 @@ import (
 	"github.com/shi-yunsheng/gostar/utils"
 )
 
+// 获取模型表名
+func getTableName(model any) string {
+	modelValue := reflect.ValueOf(model)
+	modelType := reflect.TypeOf(model)
+
+	// 处理指针类型
+	if modelValue.Kind() == reflect.Pointer {
+		if modelValue.IsNil() {
+			// 如果是 nil 指针，使用类型信息创建新实例
+			modelType = modelType.Elem()
+			modelValue = reflect.New(modelType)
+		} else {
+			modelValue = modelValue.Elem()
+			modelType = modelType.Elem()
+		}
+	}
+
+	// 获取 TableName 方法（值接收者方法）
+	tableNameMethod := modelValue.MethodByName("TableName")
+	if tableNameMethod.IsValid() {
+		// 调用 TableName() 方法
+		results := tableNameMethod.Call(nil)
+		if len(results) > 0 {
+			return results[0].String()
+		}
+	}
+
+	// 如果没有 TableName 方法，使用默认命名规则
+	return utils.CamelToSnake(modelType.Name())
+}
+
 // 联合查询
 func JoinQuery(params JoinParams, dbName ...string) (any, error) {
 	if len(params.Models) == 0 {
@@ -32,6 +63,8 @@ func JoinQuery(params JoinParams, dbName ...string) (any, error) {
 		if !ok {
 			return nil, fmt.Errorf("model [%s] not found", modelName)
 		}
+		// 获取表名（优先使用 TableName() 方法）
+		tableName := getTableName(registeredModel)
 		// 设置主表
 		if i == 0 {
 			query = query.Model(registeredModel)
@@ -42,10 +75,10 @@ func JoinQuery(params JoinParams, dbName ...string) (any, error) {
 		if len(joinConditions) > 0 && i-1 < len(joinConditions) {
 			joinCond := joinConditions[i-1]
 			// 使用解析出的连接类型和 ON 子句
-			sql = fmt.Sprintf("%s JOIN `%s` ON %s", joinCond.JoinType, tablePrefix+modelName, joinCond.OnClause)
+			sql = fmt.Sprintf("%s JOIN `%s` ON %s", joinCond.JoinType, tablePrefix+tableName, joinCond.OnClause)
 		} else {
-			// 没有连接条件时，默认使用 INNER JOIN（但这种情况应该避免）
-			sql = fmt.Sprintf("INNER JOIN `%s`", tablePrefix+modelName)
+			// 没有连接条件时，默认使用 INNER JOIN
+			sql = fmt.Sprintf("INNER JOIN `%s`", tablePrefix+tableName)
 		}
 		query = query.Joins(sql)
 	}
